6864c649e26243a163405c8688608cc00eedb4c2
zoolx92dos <zoolx92dos@gmail.com>
2024-11-14 11:04:05 +0530

    Create M13.rs

---
+++ Fundamentals/M13.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+
+// use std::intrinsics::prefetch_read_instruction;
+use std::io;
+use rand::{Rng, Error};
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use core::result;
+
+fn main() {
+    let mut st1 = String::new();
+    st1.push('A');
+    st1.push_str(" word");
+    for word in st1.split_whitespace() {
+        println!("{}", word);
+    }
+
+    let st2 = st1.replace("A", "Another");
+    println!("{}", st2);
+}

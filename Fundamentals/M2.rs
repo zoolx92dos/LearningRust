fef7d62d10f53cb227717d91200765266b5539e8
zoolx92dos <zoolx92dos@gmail.com>
2024-11-14 10:57:13 +0530

    Update M2.rs

--- Fundamentals/M2.rs
+++ Fundamentals/M2.rs
@@ -1,1 +1,19 @@
+#![allow(unused)]
 
+use std::io;
+use rand::{Rng, Error};
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use core::result;
+
+fn main() {
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.14159265;
+    let age = "47";
+    let mut age: u32 = age.trim().parse()
+        .expect("Age wasn't assigned a number");
+
+    age = age + 1;
+    println!("I'm {} and I want ${}", age, ONE_MIL);
+}

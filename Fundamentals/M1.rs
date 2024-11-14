1639f469d06dcce597a4397b6feedc862f372fa2
zoolx92dos <zoolx92dos@gmail.com>
2024-11-14 10:56:03 +0530

    Update M1.rs

--- Fundamentals/M1.rs
+++ Fundamentals/M1.rs
@@ -1,1 +1,18 @@
+#![allow(unused)]
 
+use std::io;
+use rand::{Rng, Error};
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use core::result;
+
+fn main() {
+    println!("What is your name?");
+    let mut name = String::new();
+    let greeting: &str = "Nice to meet you";
+    io::stdin().read_line(&mut name)
+        .expect("Didn't Receive Input");
+
+    println!("Hello {}! {}", name.trim_end(), greeting);
+}

c9740a5f06a51ddbb62917aa977f3512ff0df610
zoolx92dos <zoolx92dos@gmail.com>
2024-11-14 11:00:16 +0530

    Create M7.rs

---
+++ Fundamentals/M7.rs
@@ -0,0 +1,19 @@
+#![allow(unused)]
+
+// use std::intrinsics::prefetch_read_instruction;
+use std::io;
+use rand::{Rng, Error};
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use core::result;
+
+fn main() {
+    let age2 = 8;
+    match age2 {
+        1..=18 => println!("Important Birthday"),
+        21 | 50 => println!("Important Birthday"),
+        65..=i32::MAX => println!("Important Birthday"),
+        _ => println!("Not an Important Birthday"),
+    };
+}

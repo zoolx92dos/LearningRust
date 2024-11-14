575c3afafa443f682f8795ef69f46470ecf5333d
zoolx92dos <zoolx92dos@gmail.com>
2024-11-14 11:00:47 +0530

    Create M8.rs

---
+++ Fundamentals/M8.rs
@@ -0,0 +1,19 @@
+#![allow(unused)]
+
+// use std::intrinsics::prefetch_read_instruction;
+use std::io;
+use rand::{Rng, Error};
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use core::result;
+
+fn main() {
+    let my_age = 18;
+    let voting_age = 18;
+    match my_age.cmp(&voting_age) {
+        Ordering::Less => println!("Can't Vote"),
+        Ordering::Greater => println!("Can Vote"),
+        Ordering::Equal => println!("You gained the right to vote"),
+    };
+}

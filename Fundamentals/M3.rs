c1ae9c4d2a950fd8193aa659afdb3c271d4030fa
zoolx92dos <zoolx92dos@gmail.com>
2024-11-14 10:57:48 +0530

    Create M3.rs

---
+++ Fundamentals/M3.rs
@@ -0,0 +1,18 @@
+#![allow(unused)]
+
+use std::io;
+use rand::{Rng, Error};
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use core::result;
+
+fn main() {
+    // data types
+    println!("Max u32 : {}", u32::MAX);
+    println!("Max u64 : {}", u64::MAX);
+    println!("Max usize : {}", usize::MAX);
+    println!("Max u128 : {}", u128::MAX);
+    println!("Max f32 : {}", f32::MAX);
+    println!("Max f64 : {}", f64::MAX);
+}

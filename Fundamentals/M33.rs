fc5fd87ea222b1113f0a2a668cbe294ca1f629a9
zoolx92dos <zoolx92dos@gmail.com>
2024-11-14 11:17:08 +0530

    Create M33.rs

---
+++ Fundamentals/M33.rs
@@ -0,0 +1,37 @@
+#![allow(unused)]
+
+// use core::panicking::panic;
+// use std::intrinsics::prefetch_read_instruction;
+use std::io;
+use rand::{Rng, Error};
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use core::result;
+use std::collections::HashMap;
+use std::ops::Add;
+
+use std::thread;
+use std::time::Duration;
+
+// Concurrency
+/*
+Common problems with parallel programming involve:
+1. Threads are accessing data in the wrong order
+2. Threads are blocked from executing because of confusion
+   over requirements to proceed with execution
+*/
+
+fn main() {
+    thread::spawn(|| {
+        for i in 1..25 {
+            println!("Spawned thread : {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..20 {
+        println!("Main thread : {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}

4de102ae1c6c527c5fd0b799583084f8da2ad50c
zoolx92dos <zoolx92dos@gmail.com>
2024-11-14 11:09:54 +0530

    Create M22.rs

---
+++ Fundamentals/M22.rs
@@ -0,0 +1,36 @@
+#![allow(unused)]
+
+// use std::intrinsics::prefetch_read_instruction;
+use std::io;
+use rand::{Rng, Error};
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use core::result;
+
+use std::collections::HashMap;
+use std::ops::Add;
+
+// Hashmaps
+// Store key-value pairs
+
+fn main() {
+    let mut heroes = HashMap::new();
+    heroes.insert("Superman", "Clark Kent");
+    heroes.insert("Batman", "Bruce Wayne");
+    heroes.insert("The Flash", "Barry Allen");
+
+    for (k, v) in heroes.iter() {
+        println!("{} = {}", k, v);
+    }
+
+    println!("Length : {}", heroes.len());
+
+    if heroes.contains_key(&"Batman") {
+        let the_batman = heroes.get(&"Batman");
+        match the_batman {
+            Some(x) => println!("Batman is a hero"),
+            None => println!("Batman is not a hero"),
+        }
+    }
+}
